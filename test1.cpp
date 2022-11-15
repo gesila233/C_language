f2b7e8ad4143639061f88548200ee5ef5c834c02
gesila233 <116056765+gesila233@users.noreply.github.com>
2022-11-15 11:27:13 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,51 @@
+//#include<stdio.h>
+//int main()
+//{
+//	char a='A';
+//	a+=32;
+//	printf("%c\n",a);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a =5,b,c;
+//	b=a+2;
+//	c=b-1;
+//	printf("a=%d,b=%d,c=%d\n",a,b,c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a;
+//	printf("请输入一个大写字母：\n");
+//	a=getchar();
+//	a=a+32;
+//	printf("它的小写字母是：\n");
+//	putchar(a);
+//	putchar('\n');
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	float a=-3.14,b;
+//	b=fabs(a);
+//	printf("%f\n",b);
+//	return 0;
+//}
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a; 
+	a=exp(2);
+	printf("%f\n",a);
+	return 0;
+}
